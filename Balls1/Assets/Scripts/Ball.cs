a1ca38fee052b50b3986345c3d7015daafb49bdc
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-03-22 22:32:34 +0000

    Initial code commit

---
+++ Balls1/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private BallSet ballSet_ = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	public void Init(BallSet bs)
+	{
+		ballSet_ = bs;
+		Material myMaterial = new Material (ballSet_.ballMaterial);
+		GetComponent< MeshRenderer > ().material = myMaterial;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Balls1/Assets/Scripts/BallSet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSet : MonoBehaviour {
+
+	static int s_nextBallNumber = 0;
+
+	public Transform ballSpawnPoint;
+	public GameObject ballPrefab;
+	public Transform ballsContainer;
+
+	public Material ballMaterial;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpawnBall()
+	{
+		GameObject newObject = Instantiate (ballPrefab) as GameObject;
+		newObject.name = "Ball" + s_nextBallNumber.ToString ();
+		newObject.transform.position = ballSpawnPoint.transform.position;
+		newObject.transform.parent = ballsContainer;
+
+		Ball newBall = newObject.GetComponent< Ball > ();
+		newBall.Init (this);
+
+		s_nextBallNumber++;
+	}
+}
