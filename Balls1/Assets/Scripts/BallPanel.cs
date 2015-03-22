b91c5fcde0ed49f5c068d84147526ea2d6ff1f74
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-03-22 23:19:27 +0000

    Touching balls, BallPanel & GameManager

---
+++ Balls1/Assets/Scripts/BallPanel.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallPanel : SingletonSceneLifetime < BallPanel > 
+{
+	private Ball currentBall_ = null;
+	public Text headingText;
+
+	// Use this for initialization
+	void Start () {
+		Close ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetBall( Ball b)
+	{
+		currentBall_ = b;
+		if (currentBall_ != null) 
+		{
+			headingText.text = b.gameObject.name;
+		} 
+		else 
+		{
+			headingText.text = "NONE";
+		}
+	}
+
+	public void Open(Ball b)
+	{
+		SetBall (b);
+		Open ();
+	}
+
+	public void Open()
+	{
+		gameObject.SetActive (true);
+	}
+
+	public void Close()
+	{
+		gameObject.SetActive (false);
+	}
+}
---
+++ Balls1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : SingletonSceneLifetime < GameManager > 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		BallPanel.Instance.Close ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButton (0)) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 200))
+			{
+				Transform t = hit.transform;
+				Debug.Log("Ray hit "+t.gameObject.name); 
+
+				Ball ball = t.gameObject.GetComponent< Ball >();
+				if (ball != null)
+				{
+					BallPanel.Instance.Open(ball);
+				}
+			}          
+		}
+	}
+}
